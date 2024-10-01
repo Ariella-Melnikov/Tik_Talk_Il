87243c58a95743fa409ca810db46ecd1f8778316
Ariella Melnikov <melnikov.ariella@gmail.com>
2024-10-01 12:25:36 +0300

    fix style problems in the table and quiz

--- tik_talk_il/src/cmps/AppFooter.vue
+++ tik_talk_il/src/cmps/AppFooter.vue
@@ -1,17 +1,62 @@
 <template>
-	<footer>
-        <p>Coffee Rights 2024</p>
-	</footer>
+    <footer>
+        <div class="social-links">
+            <h2 class="footer-text">TIK TALK Make English Easy</h2>
+            <a
+                href="https://www.instagram.com/_tik_talk_il_?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
+                target="_blank"
+                rel="noopener noreferrer">
+                <font-awesome-icon :icon="['fab', 'instagram']" class="social-icon" />
+            </a>
+            <a href="https://www.linkedin.com/in/shoham-joseph-835b091b4/" target="_blank" rel="noopener noreferrer">
+                <font-awesome-icon :icon="['fab', 'linkedin']" class="social-icon" />
+            </a>
+        </div>
+    </footer>
 </template>
 
 <style lang="scss">
 footer {
+    margin-top: auto;
     display: flex;
     align-items: center;
     justify-content: center;
-    height: 5vh;
+    padding: 1rem 0;
+    background-image: url('@/assets/img/header_background.png');
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    transition: background-color 0.3s ease;
+}
 
-    padding: 10px;
-    background-color: lightblue;
+.social-links {
+    display: flex;
+    align-items: center;
+    gap: 1.5rem;
+    justify-content: center;
+    transition: transform 0.3s ease;
+
+    &:hover {
+        transform: scale(1.1);
+    }
+}
+
+.footer-text {
+    font-size: 1rem;
+    color: #2d2a6c;
+    margin: 0;
+    line-height: 1;
+    display: inline-block;
+    padding-right: 0.5rem;
+}
+
+.social-icon {
+    font-size: 2rem; /* Adjust the size of the icons */
+    color: #0e0d0d;
+    transition: color 0.3s ease;
+
+    &:hover {
+        color: #4c68d7; /* Change icon color on hover */
+    }
 }
 </style>
--- tik_talk_il/src/cmps/QuestionPreview.vue
+++ tik_talk_il/src/cmps/QuestionPreview.vue
@@ -1,22 +1,26 @@
 <template>
     <div class="question-card">
         <p class="question-text">{{ question.text }}</p>
-        <div v-for="(option, index) in question.options" :key="index">
-            <input
-                type="radio"
-                :name="'question' + question._id"
-                :id="'q' + question._id + 'o' + index"
-                :value="option"
-                v-model="selectedAnswer"
-                :disabled="answerSubmitted" />
-            <label :class="getOptionClass(option)" class="answer-text" :for="'q' + question._id + 'o' + index">
-                {{ option }}
-            </label>
+        <div class="answer-grid">
+            <div v-for="(option, index) in question.options" :key="index" class="answer">
+                <input
+                    type="radio"
+                    :name="'question' + question._id"
+                    :id="'q' + question._id + 'o' + index"
+                    :value="option"
+                    v-model="selectedAnswer"
+                    :disabled="answerSubmitted" />
+                <label :class="getOptionClass(option)" class="answer-text" :for="'q' + question._id + 'o' + index">
+                    {{ option }}
+                </label>
+            </div>
         </div>
-        <button v-if="!answerSubmitted" @click="submitAnswer" class="action-button" :disabled="!selectedAnswer">
-            {{ $t('quiz.submit') }}
-        </button>
-        <button v-if="answerSubmitted" @click="nextQuestion" class="action-button">{{ 'Next' }}</button>
+        <div class="action-buttons">
+            <button v-if="!answerSubmitted" @click="submitAnswer" class="action-button" :disabled="!selectedAnswer">
+                {{ $t('quiz.submit') }}
+            </button>
+            <button v-if="answerSubmitted" @click="nextQuestion" class="action-button">{{ $t('quiz.next') }}</button>
+        </div>
     </div>
 </template>
 
@@ -63,8 +67,10 @@ export default {
     border-radius: 10px;
     box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
     padding: 1.5rem;
-    margin: 1rem 0;
     transition: transform 0.2s ease-in-out, box-shadow 0.2s ease-in-out;
+    text-align: left; /* Ensures left alignment for text */
+    margin: 1rem 0;
+    direction: ltr;
 
     &:hover {
         transform: translateY(-5px);
@@ -75,10 +81,33 @@ export default {
         font-size: 2rem;
         font-weight: bold;
         margin-bottom: 1rem;
+        text-align: center;
+    }
+    .answer-grid {
+        display: grid;
+        grid-template-columns: auto 1fr; /* Auto width for radio buttons, 1fr for text */
+        align-items: center;
+        gap: 0.5rem 1rem; /* Row gap and column gap */
+        margin: 0.5rem 0;
     }
 
-    .answer-text {
-        font-size: 1.5rem; // Make the answer text slightly larger
+    .answer {
+        display: contents; /* Ensures the child elements participate in the grid layout */
+
+        input[type='radio'] {
+            margin: 0; /* Remove default margin */
+            justify-self: center; /* Align radio button to the right of the first column */
+            cursor: pointer;
+            transform: scale(1.5); /* Optional: Make radio buttons slightly bigger */
+        }
+
+        .answer-text {
+            font-size: 1.5rem;
+            margin-inline-start: 0.5rem;
+            padding: 0;
+            justify-self: start; /* Align text to the left of the second column */
+            cursor: pointer; /* Change cursor to pointer */
+        }
     }
 }
 
@@ -90,6 +119,13 @@ export default {
 .incorrect {
     color: red;
     font-weight: bold;
+}
+
+.action-buttons {
+    display: flex;
+    justify-content: center;
+    gap: 1rem;
+    margin-top: 1.5rem;
 }
 
 .action-button {
